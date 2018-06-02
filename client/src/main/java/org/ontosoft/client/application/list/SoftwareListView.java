8276bef21d58c36981ca6fe1ba32f1f6b51374a4
Varun Ratnakar <varunratnakar@gmail.com>
2018-06-02 09:01:30 +0530

    Removing maven warnings

--- client/src/main/java/org/ontosoft/client/application/list/SoftwareListView.java
+++ client/src/main/java/org/ontosoft/client/application/list/SoftwareListView.java
@@ -491,6 +491,7 @@ public class SoftwareListView extends ParameterizedViewImpl
     Collections.sort(allSoftwareList, swcompare);    
   }
   
+  @SuppressWarnings("unlikely-arg-type")
   private void removeFromList(SoftwareSummary summary) {
     filteredSoftwareIdMap.remove(summary);
     allSoftwareList.remove(summary);
